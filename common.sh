555b762167798f201500791dcbe43e85840880b9
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2016-03-30 03:13:49 +0300

    SYMLINK_PATH variable is added

--- common.sh
+++ common.sh
@@ -5,3 +5,14 @@ pause () {
     fi
     read -p "$PAUSE_MSG"
 }
+
+SYMLINK_PATH=$(dirname $(readlink -f "$0"))
+
+silent_run () {
+    #echo "DEBUG: $*"
+
+    # Run in background: 
+    #nohup "$*" >/dev/null 2>&1 &
+    # "Extra safe" version:
+    nohup "$*" </dev/null >/dev/null 2>&1 &
+}
