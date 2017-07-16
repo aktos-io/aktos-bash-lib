ff631af15bb04805b34659fd0028bacf2ed7151a
Cerem Cem ASLAN <cem@aktos.io>
2017-07-16 15:03:33 +0300

    fixed ssh socket run function names, added get_fingerprint function

--- ssh-functions.sh
+++ ssh-functions.sh
@@ -12,12 +12,15 @@ SSHFS="sshfs -o reconnect,ServerAliveInterval=60,ServerAliveCountMax=3"
 
 [ $SSH_SOCKET_FILE ] || SSH_SOCKET_FILE="/tmp/ssh-$SSH_USER@$SSH_HOST:$SSH_PORT.sock"
 
-ssh_run_via_socket () {
+ssh_socket_run_cmd () {
     [ $SSH_HOST ] || SSH_HOST=$(echo $SSH_SOCKET_FILE | cut -d@ -f2 | cut -d':' -f1)
-    $SSH -N -S $SSH_SOCKET_FILE $SSH_HOST $@ &
-    # ssh -S socket spawns a child process, so add 1 to get actual pid
-    last_pid=$(( $! + 1))
+    $SSH -S $SSH_SOCKET_FILE $SSH_HOST $@
 }
+
+ssh_socket_make_forward () {
+    ssh_socket_run_cmd -N $@
+}
+
 
 parse_url () {
     local section=$1
@@ -88,4 +91,10 @@ ssh_socket_command () {
 get_public_key () {
     local private_key=$1
     ssh-keygen -y -f $private_key
+}
+
+get_fingerprint () {
+    local str=$@
+    local fingerprint=$(echo $(bash -c "ssh-keygen -E md5 -l -f /dev/stdin <<<'$str'") | cut -d ' ' -f 2)
+    echo ${fingerprint#'MD5:'}
 }
