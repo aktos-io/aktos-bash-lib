618b87058338b311b68a80cfe97baaa39c4304c2
root <root@aea3.aktos.io>
2018-08-11 14:28:29 -0400

    fixed unbound exception

--- ssh-functions.sh
+++ ssh-functions.sh
@@ -10,7 +10,7 @@ SSHFS="sshfs -o reconnect,ServerAliveInterval=60,ServerAliveCountMax=3"
 # * SSH_KEY_FILE
 # * SSH_PATH
 
-[ ${SSH_SOCKET_FILE:-} ] || SSH_SOCKET_FILE="/tmp/ssh-$SSH_USER@$SSH_HOST:$SSH_PORT.sock"
+[ ${SSH_SOCKET_FILE:-} ] || SSH_SOCKET_FILE="/tmp/ssh-${SSH_USER:-foo}@${SSH_HOST:-example.com}:${SSH_PORT:-22}.sock"
 
 ssh_socket_run_cmd () {
     # SSH does not care the hostname while using socket file
