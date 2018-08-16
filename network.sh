6e4e82b4d2dfbc2dd21c45cca1bbf0ea21a87f67
Cerem Cem ASLAN <cem@aktos.io>
2018-08-17 02:59:52 +0300

    added is_cable_plugged function

--- network.sh
+++ network.sh
@@ -1,8 +1,17 @@
 is_network_reachable() {
-   /bin/ping -c1 -w1 8.8.8.8 &> /dev/null
+    # returns: boolean
+    /bin/ping -c1 -w1 8.8.8.8 &> /dev/null
 }
 
 get_ip_of(){
+    # returns: string
     local iface=$1
     ifconfig $iface | egrep "inet\W" | awk '{print $2}'
+}
+
+is_cable_plugged() {
+    # returns: boolean
+    # true if cable is plugged, else false
+    local iface=$1
+    [[ `ifconfig $iface | sed -n '/running/I p'` != '' ]]
 }
