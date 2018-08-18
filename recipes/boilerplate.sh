ee3f279678248479852ba762695b4a1f03bfcccb
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2018-08-18 18:20:35 +0300

    Update boilerplate.sh

--- recipes/boilerplate.sh
+++ recipes/boilerplate.sh
@@ -10,9 +10,6 @@ safe_source () { [[ ! -z ${1:-} ]] && source $1; _dir="$(cd "$(dirname "${BASH_S
 # source another bash file without changin the "magic" variables
 safe_source /path/to/bash/file
 
-# All checks are done, run as root.
-[[ $(whoami) = "root" ]] || { sudo $0 $*; exit 0; }
-
 # iterate over directory contents 
 for file in Data/*.txt; do
     [ -e "$file" ] || continue
@@ -30,3 +27,52 @@ while read -r sub; do
      #do work
     echo $sub
 done <<< `btrfs sub list / -R`
+
+# show help
+show_help(){
+    local script=$(basename $0)
+    local reason=${1:-}
+    [[ ! -z $reason ]] && cat <<REASON
+    -------------------------------
+    ERROR: $reason
+    -------------------------------
+REASON
+    cat <<HELP
+
+    $script [options] /path/to/source /path/to/destination
+
+    Options:
+
+        --dry-run       : Dry run, don't touch anything actually
+
+HELP
+    exit
+}
+
+
+# parsing command line arguments 
+POSITIONAL=()
+_count=1
+while :; do
+    POSITIONAL+=("$1") # save for "sudo $0" usage
+    key="${1:-}"
+    case $key in
+        -h|-\?|--help|'')
+            show_help    # Display a usage synopsis.
+            exit
+            ;;
+        --dry-run)       # Takes an option argument; ensure it has been specified.
+            shift
+            dry_run=true
+            ;;
+        *)  # save the positional arguments
+            declare _arg$((_count++))="$1"
+            shift
+    esac
+    [[ -z ${1:-} ]] && break
+done
+set -- "${POSITIONAL[@]}"
+# use $_arg1 in place of $1, $_arg2 in place of $2 and so on, "$@" is intact
+
+# All checks are done, run as root.
+[[ $(whoami) = "root" ]] || { sudo $0 "$@"; exit 0; }
