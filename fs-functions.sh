e3e94245d6321287474792b5a0c79564e8213b98
Cerem Cem ASLAN <cem@aktos.io>
2018-12-31 11:39:11 +0300

    added get-latest-folder function

--- fs-functions.sh
+++ fs-functions.sh
@@ -96,6 +96,43 @@ exec_limited () {
 	return $?
 }
 
+# Query related
+get-latest-folder(){
+    # Taken from https://stackoverflow.com/a/26766782/1952991
+    #
+    # Use ${1-} instead of $1 in case 'nounset' is set
+    local -r glob_pattern=${1-}
+
+    if (( $# != 1 )) ; then
+        echo 'usage: newest_matching_file GLOB_PATTERN' >&2
+        return 1
+    fi
+
+    # To avoid printing garbage if no files match the pattern, set
+    # 'nullglob' if necessary
+    local -i need_to_unset_nullglob=0
+    if [[ ":$BASHOPTS:" != *:nullglob:* ]] ; then
+        shopt -s nullglob
+        need_to_unset_nullglob=1
+    fi
+
+    newest_file=
+    for file in $glob_pattern/* ; do
+        newest_file=$file
+    done
+
+    # To avoid unexpected behaviour elsewhere, unset nullglob if it was
+    # set by this function
+    (( need_to_unset_nullglob )) && shopt -u nullglob
+
+    # Use printf instead of echo in case the file name begins with '-'
+    [[ -n $newest_file ]] && printf '%s\n' "$newest_file"
+
+    return 0
+}
+
+
+
 # Physical disk related
 find_disks () {
     fdisk -l 2>/dev/null \
